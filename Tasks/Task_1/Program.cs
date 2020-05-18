32506a82a5488222b6c23cda54e5123a833001df
Данил <prowokatorkraft@gmail.com>
2020-05-19 01:45:41 +0400

    Task finished succesfully

--- Tasks/Task_1/Program.cs
+++ Tasks/Task_1/Program.cs
@@ -2,11 +2,59 @@
 
 namespace Task_1
 {
+    // 1.1.1	RECTANGLE
+    // Написать программу, которая определяет площадь прямоугольника со сторонами a и b.
+    // Если пользователь вводит некорректные значения (отрицательные или ноль), 
+    // должно выдаваться сообщение об ошибке. Возможность ввода пользователем 
+    // строки вида «абвгд» или нецелых чисел игнорировать.
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Rectangle area");
+            Console.WriteLine(new string('-', 20));
 
+
+            Console.Write("Enter side A: ");
+
+            if (Rectangle.SetSide(out Rectangle.a))
+            {
+                Console.Write("Enter side B: ");
+
+                if (Rectangle.SetSide(out Rectangle.b))
+                {
+                    Console.WriteLine(new string('-', 20));
+                    Console.WriteLine("Rectangle area = " + Rectangle.GetAria());
+                }
+                else Console.WriteLine("Incorrect enter!");
+            }
+            else Console.WriteLine("Incorrect enter!");
+                
+
+            Console.ReadKey();
+        }
+
+
+
+        static class Rectangle
+        {
+            public static int a, b;
+
+            public static bool SetSide(out int side)
+            {
+                string value = Console.ReadLine();
+
+                if (int.TryParse(value, out side) && side > 0)
+                    return true;
+                else
+                    return false;
+            }
+
+            public static int GetAria()
+            {
+                return a * b;
+            }
         }
     }
 }
