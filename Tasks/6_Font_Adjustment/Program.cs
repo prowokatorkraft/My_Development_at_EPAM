0d7eb2ebac5d514e9eb75356177c74076f96ce4f
Данил <prowokatorkraft@gmail.com>
2020-05-20 14:13:22 +0400

    Finished Task 1.1.6

---
+++ Tasks/6_Font_Adjustment/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6_Font_Adjustment
+{
+    // 1.1.6    Font Adjustment
+    // Для форматирования текста надписи можно использовать различные начертания:
+    // полужирное, курсивное и подчёркнутое, а также их сочетания.
+    // Предложите способ хранения информации о форматировании текста надписи
+    // и напишите программу, которая позволяет устанавливать и изменять начертание:
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Font Adjustment");
+            
+            Font font = new Font(false, false, false);
+            int enter;
+
+            while (true)
+            {
+                Console.WriteLine(new string('-', 40));
+
+                Console.WriteLine("Параметры надписи: " + GetFonts(font));
+
+                Console.WriteLine(
+                                   "Введите:" +
+                                   "\n\t1: bold" +
+                                   "\n\t2: italic" +
+                                   "\n\t3: bold"
+                                  );
+
+                if (int.TryParse(Console.ReadLine(), out enter) && enter < 4)
+                {
+                    if (enter == 0) return;  // Для выхода из приложения
+
+                    ChangeFonts(enter, ref font);
+                }
+                else Console.WriteLine("Incorrect enter!");
+            }
+        }
+
+        static string GetFonts(Font font)
+        {
+            if (font.bold)
+            {
+                if (font.italic)
+                {
+                    if (font.underline)
+                        return "bold, italic, underline";
+                    else
+                        return "bold, italic";
+                }
+                else if (font.underline)
+                    return "bold, underline";
+                else
+                    return "bold";
+            }
+            else if (font.italic)
+            {
+                if (font.underline)
+                    return "italic, underline";
+                else
+                    return "italic";
+            }
+            else if (font.underline)
+                return "underline";
+            
+            return "none";
+        }
+        static void ChangeFonts(int setFont, ref Font font)
+        {
+            switch (setFont)
+            {
+                case 1:
+                    font.bold = !font.bold;
+                    break;
+                case 2:
+                    font.italic = !font.italic;
+                    break;
+                case 3:
+                    font.underline = !font.underline;
+                    break;
+                default:
+                    throw new ArgumentException("Incorrect argument setFont! (1-3)");
+            }
+        }
+    }
+
+    struct Font
+    {
+        public bool bold { get; set; }
+        public bool italic { get; set; }
+        public bool underline { get; set; }
+
+        public Font(bool bold, bool italic, bool underline)
+        {
+            this.bold = bold;
+            this.italic = italic;
+            this.underline = underline;
+        }
+    }
+}
---
+++ Tasks/6_Font_Adjustment/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("6_Font_Adjustment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6_Font_Adjustment")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("72af6cb3-60f2-4976-8f09-e042de2783be")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
