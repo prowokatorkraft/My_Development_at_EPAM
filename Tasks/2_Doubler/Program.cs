6e457d22440c5dc5d230f1de89afa162aef435c6
Данил <prowokatorkraft@gmail.com>
2020-05-21 23:18:13 +0400

    Finished Task 1.2.2

---
+++ Tasks/2_Doubler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_Doubler
+{
+    // 1.2.2    Doubler
+    // Напишите программу, которая удваивает в первой введённой 
+    // строке все символы, принадлежащие второй введённой строке.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doubler");
+            Console.WriteLine(new string('-', 25));
+
+            char[] simbols1;
+            Console.Write("Enter text 1: ");
+            simbols1 = Console.ReadLine().ToCharArray();
+
+            char[] simbols2;
+            Console.Write("Enter text 2: ");
+            simbols2 = Console.ReadLine().Distinct().ToArray();
+
+            Console.WriteLine(new string('-', 25));
+
+            for (int i1 = 0; i1 < simbols1.Length; i1++)
+            {
+                for (int i2 = 0; i2 < simbols2.Length; i2++)
+                {
+                    if (simbols1[i1] == simbols2[i2])
+                    {
+                        Console.Write(simbols1[i1]);
+                    }
+                }
+
+                Console.Write(simbols1[i1]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
