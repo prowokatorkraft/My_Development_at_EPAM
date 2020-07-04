9e91a29079a4ec4127ae2c5164b67058f63ca98a
Данил <prowokatorkraft@gmail.com>
2020-07-05 01:51:23 +0400

    Added logging to task 3.1.1

--- Tasks_3/3.1.1_Weakest_Link/Program.cs
+++ Tasks_3/3.1.1_Weakest_Link/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace _3._1._1_Weakest_Link
 {
@@ -7,6 +8,11 @@ namespace _3._1._1_Weakest_Link
     {
         static void Main(string[] args)
         {
+            Debug.AutoFlush = true;
+            Debug.Listeners.Add(new TextWriterTraceListener(System.IO.File.CreateText("Log.txt")));
+            Debug.WriteLine("Debug Information-Product Starting ");
+            Debug.Indent();
+
             while (true)
             {
                 int _lenght = 0;
@@ -16,15 +22,22 @@ namespace _3._1._1_Weakest_Link
 
                 Console.WriteLine("Введите N:");
 
-                int.TryParse(Console.ReadLine(), out _lenght);
+                Debug.WriteLineIf(
+                    int.TryParse(Console.ReadLine(), out _lenght), 
+                    $"Initialized _lenght ({_lenght})", "Info");
 
                 Console.WriteLine("Введите, какой по счету человек будет вычеркнут каждый раунд:");
 
-                int.TryParse(Console.ReadLine(), out _coef);
+                Debug.WriteLineIf(
+                    int.TryParse(Console.ReadLine(), out _coef), 
+                    $"Initialized _coef ({_coef})", "Info");
 
 
                 if (_lenght < 1 || _coef < 1)
                 {
+                    Debug.WriteIf(_lenght < 1, "invalid value entered _lenght", "Warm");
+                    Debug.WriteIf(_coef < 1, "invalid value entered _lenght", "Warm");
+
                     continue;
                 }
 
@@ -32,10 +45,13 @@ namespace _3._1._1_Weakest_Link
                 {
                     _people.Add(i);
                 }
+                Debug.WriteLine($"{_lenght} items added to _people", "Info");
 
                 Console.WriteLine($"Сгенерирован круг людей: {_lenght}. Начинаем вычеркивать каждого {_coef}");
 
 
+                Debug.WriteLine("Enumerated items started", "Info");
+                Debug.Indent();
                 for (int round = 0, index = 0, count = 0; _coef <= _people.Count; index++, count++)
                 {
                     if (count == _coef - 1)
@@ -47,13 +63,18 @@ namespace _3._1._1_Weakest_Link
                         count = -1;
 
                         Console.WriteLine($"Раунд {round}. Вычеркнут человек. Людей осталось: {_people.Count}");
+                        Debug.WriteLine($"Removed item (round: {round}, index: {index}, _people.Count: {_people.Count})");
                     }
 
                     if (index >= _people.Count - 1)
                     {
                         index = -1;
+
+                        Debug.WriteLine($"The index is dumped (round: {round}, count: {count})");
                     }
                 }
+                Debug.Unindent();
+                Debug.WriteLine("Enumerated items completed", "Info");
 
                 Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей");
                 Console.ReadKey();
