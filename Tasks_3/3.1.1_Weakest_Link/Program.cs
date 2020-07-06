6ae2bafa9200707de0b6fbe5f58039c2085dc78a
Данил <prowokatorkraft@gmail.com>
2020-07-06 22:02:31 +0400

    Refactored Task 3.1.1

--- Tasks_3/3.1.1_Weakest_Link/Program.cs
+++ Tasks_3/3.1.1_Weakest_Link/Program.cs
@@ -7,58 +7,67 @@ namespace _3._1._1_Weakest_Link
     {
         static void Main(string[] args)
         {
+            int _lenght;
+            int _coef;
+            List<int> _people;
+
             while (true)
             {
-                int _lenght = 0;
-                int _coef = 0;
-                List<int> _people = new List<int>();
-
+                _lenght = 0;
+                _coef = 0;
+                _people = new List<int>();
 
                 Console.WriteLine("Введите N:");
-
                 int.TryParse(Console.ReadLine(), out _lenght);
 
                 Console.WriteLine("Введите, какой по счету человек будет вычеркнут каждый раунд:");
-
                 int.TryParse(Console.ReadLine(), out _coef);
-
 
                 if (_lenght < 1 || _coef < 1)
                 {
                     continue;
                 }
 
-                for (int i = 0; i < _lenght; i++)
-                {
-                    _people.Add(i);
-                }
+                InitializeList(_people, _lenght);
 
                 Console.WriteLine($"Сгенерирован круг людей: {_lenght}. Начинаем вычеркивать каждого {_coef}");
 
-
-                for (int round = 0, index = 0, count = 0; _coef <= _people.Count; index++, count++)
-                {
-                    if (count == _coef - 1)
-                    {
-                        round++;
-
-                        _people.RemoveAt(index);
-
-                        count = -1;
-
-                        Console.WriteLine($"Раунд {round}. Вычеркнут человек. Людей осталось: {_people.Count}");
-                    }
-
-                    if (index >= _people.Count - 1)
-                    {
-                        index = -1;
-                    }
-                }
+                CrossElementsOut<int>(_people, _coef);
 
                 Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей");
                 Console.ReadKey();
 
                 Console.Clear();
+            }
+        }
+
+        private static void InitializeList(List<int> people, int lenght)
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                people.Add(i);
+            }
+        }
+
+        private static void CrossElementsOut<T>(List<T> array, int coefficient)
+        {
+            for (int round = 0, index = 0, count = 0; coefficient <= array.Count; index++, count++)
+            {
+                if (count == coefficient - 1)
+                {
+                    round++;
+
+                    array.RemoveAt(index);
+
+                    count = -1;
+
+                    Console.WriteLine($"Раунд {round}. Вычеркнут человек. Людей осталось: {array.Count}");
+                }
+
+                if (index >= array.Count - 1)
+                {
+                    index = -1;
+                }
             }
         }
     }
