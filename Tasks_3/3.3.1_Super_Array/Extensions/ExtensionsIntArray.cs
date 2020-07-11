db3c7f2128b77b853f9838f9479311150b16e741
Данил <prowokatorkraft@gmail.com>
2020-07-11 19:14:22 +0400

    Finished Task 3.3.1

---
+++ Tasks_3/3.3.1_Super_Array/Extensions/ExtensionsIntArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._3._1_Super_Array.Extensions
+{
+    internal static class ExtensionsIntArray
+    {
+        public static T Act<T>(this int[] values, Func<int[], T> func)
+        {
+            return func(values);
+        }
+        public static void Act(this int[] values, Action<int[]> func)
+        {
+            func(values);
+        }
+
+        public static int TotalSum(this int[] values)
+        {
+            return values.Sum();
+        }
+
+        public static int AverageValue(this int[] values)
+        {
+            return (int) values.Average();
+        }
+
+        public static int FrequentValue(this int[] values)
+        {
+            int value = 0;
+            int count = -1;
+
+            int newCount = -1;
+
+            foreach (var item in values)
+            {
+                newCount = values.Count(e => e.Equals(item));
+
+                if (newCount > count)
+                {
+                    value = item;
+                    count = newCount;
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Tasks_3/3.3.1_Super_Array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using _3._3._1_Super_Array.Extensions;
+
+namespace _3._3._1_Super_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 1,2,3,4,5,6,10,1,6,6 };
+
+            Console.WriteLine(test.FrequentValue());
+
+            Console.ReadKey();
+        }
+    }
+}
