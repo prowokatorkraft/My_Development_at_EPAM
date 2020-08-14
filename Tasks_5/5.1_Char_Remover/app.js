001a3b025556cd085564cc79d6769a7bc06f5c4f
Данил <prowokatorkraft@gmail.com>
2020-08-14 21:30:21 +0400

    Finished Task 5.1

---
+++ Tasks_5/5.1_Char_Remover/app.js
@@ -0,0 +1,65 @@
+function RemoveRepeatingChars(str) {
+
+    if (typeof (str) != "string") {
+
+        return null;
+    }
+
+    let arrayChar = new Array(0);
+
+    FindRepeatingChar();
+
+    return DeletedChars();
+
+    function IsRepeatingChar(ch, world, reiteration) {
+
+        for (let i in world) {
+
+            if (ch === world[i]) {
+
+                reiteration--;
+
+                if (reiteration <= -1) {
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    function FindRepeatingChar() {
+
+        let strArray = str.split(" ");
+
+        for (let world in strArray) {
+
+            for (let ch in strArray[world]) {
+
+                if (IsRepeatingChar(strArray[world][ch], strArray[world], 1)) {
+
+                    if (-1 == arrayChar.findIndex((value, index, array) => value === strArray[world][ch])) {
+
+                        arrayChar.push(strArray[world][ch]);
+                    }
+                }
+            }
+        }
+    }
+
+    function DeletedChars() {
+
+        let newStr = "";
+
+        for (let iCh = 0; iCh < str.length; iCh++) {
+
+            if (arrayChar.findIndex((value, index, array) => value === str[iCh]) == -1) {
+
+                newStr += str[iCh];
+            }
+        }
+
+        return newStr;
+    }
+}
