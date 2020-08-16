7cbac64c0ed1b782771d54249ec28f1c7e25eecf
Данил <prowokatorkraft@gmail.com>
2020-08-16 19:41:45 +0400

    Finished Task 5.3

---
+++ Tasks_5/5.3_Mini_Crud/library.js
@@ -0,0 +1,71 @@
+﻿let sourse = new Map();
+
+function getId() {
+
+    return (Math.random() * 0xffffffff | 0).toString();
+}
+
+module.exports = {
+
+    add: function (obj) {
+
+        let id = getId();
+
+        sourse.set(id, obj);
+
+        return id;
+    },
+    getById: function getById(id) {
+
+        if (sourse.has(id)) {
+
+            return sourse.get(id);
+        }
+
+        return null;
+    },
+    getAll: function () {
+
+        return sourse.values();
+    },
+    deleteById: function (id) {
+
+        if (sourse.has(id)) {
+
+            let obj = sourse.get(id);
+
+            sourse.delete(id);
+
+            return obj;
+        }
+
+        return null;
+    },
+    updateById: function (id, obj) {
+
+        if (sourse.has(id) && typeof (obj) == "object") {
+
+            let oldObj = sourse.get(id);
+
+            for (var item in obj) {
+
+                oldObj[item] = obj[item];
+            }
+
+            return true;
+        }
+
+        return false;
+    },
+    replaceById: function (id, obj) {
+
+        if (sourse.has(id)) {
+
+            sourse.set(id, obj);
+
+            return true;
+        }
+
+        return false;
+    }
+};
